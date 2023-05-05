635528e47a49fa806ed6370e58ce5fcc9ba03da6
Victor <vict02r05@gmail.com>
2023-05-05 17:56:40 +0300

    add loader to filters block

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -11,27 +11,42 @@ import { activeFilterChanged } from '../../actions';
 // Представьте, что вы попросили бэкенд-разработчика об этом
 
 const HeroesFilters = () => {
-  const { filters, activeFilter } = useSelector(state => state.filters);
+  const { filters, activeFilter, filtersLoadingStatus } = useSelector(
+    state => state.filters
+  );
   const dispatch = useDispatch();
+
+  const loader =
+    filtersLoadingStatus === 'loading' ? (
+      <div>Загрузка элементов...</div>
+    ) : null;
+  const error =
+    filtersLoadingStatus === 'error' ? <div>Ошибка загрузки</div> : null;
+  const content =
+    filtersLoadingStatus === 'idle'
+      ? filters.map(({ value, className, name }) => {
+          return (
+            <button
+              onClick={() => dispatch(activeFilterChanged(value))}
+              key={value}
+              className={classNames('btn', `${className}`, {
+                active: value === activeFilter
+              })}
+            >
+              {name}
+            </button>
+          );
+        })
+      : null;
 
   return (
     <div className="card shadow-lg mt-4">
       <div className="card-body">
         <p className="card-text">Отфильтруйте героев по элементам</p>
         <div className="btn-group">
-          {filters.map(({ value, className, name }) => {
-            return (
-              <button
-                onClick={() => dispatch(activeFilterChanged(value))}
-                key={value}
-                className={classNames('btn', `${className}`, {
-                  active: value === activeFilter
-                })}
-              >
-                {name}
-              </button>
-            );
-          })}
+          {loader}
+          {error}
+          {content}
         </div>
       </div>
     </div>
